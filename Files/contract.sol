e3442ba498ff3ddc0c8e10da7631a7c06c041da5
Abdellah Ghassel <abdellah.ghassel@queensu.ca>
2022-05-29 01:40:59 -0400

    Add files via upload

---
+++ Files/contract.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+contract Sponsorship {
+  uint256 storedData;
+	
+  function get() public view returns (uint) {
+    return storedData;
+  }
+
+  if (FinancialCriteria = 40000) {
+  Finances = true;
+  } else {
+  Finances = false;
+  }
+  
+  if (SupplyCriteria= true){
+    clothing = true;
+    food = true; 
+    housing = true;
+    publicServices = true;
+  }
+  else {
+    clothing = false;
+    food = false; 
+    housing = false;
+    publicServices = false;
+  }
+
+  if (age >=18){
+    agelimit = true
+  }
+
+  else {agelimist= false}
+
+  if (criminal record = false){
+    safe = true
+  }
+  else{safe = false}
+  if (citizenship= true){
+    citizen = true
+  }
+  else {citizen = false}
+  }
+
+if {citizen && agelimit && publicServices && housing && food && clothing && Finances = true}
+
+passCondition = true;
+
+require("The Sponsor meeets the criteria to be able to house refugees");
+}
+
+else {passCondition = false; require("The Sponsor does not meet the codition of the refugee");}
